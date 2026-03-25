0ed4d198134d7ef5a42e23cc91adb5b3dc98bc49
ReabetsweJankie <jankiereabetswe@gmail.com>
2026-03-25 16:15:48 +0200

    last commit for binary search tree

--- tryBST.java
+++ tryBST.java
@@ -1,98 +1,128 @@
 //created a java file
 import java.util.*;
 
-class TNode{
+class tNode{
     int key;
-    TNode left, right;
+    tNode left, right;
     
-    public TNode(int key){
+    public tNode(int key){
         this.key = key;
-        this.left = null;
-        this.right = null;
+        left = null;
+        right = null;
     }
-}                      // Defined tree node structure
+}             // Defined tree node structure
+
 class BST{
-    
-    TNode root;
-    
-    public BST(){
-        this.root = null;
+    tNode root;
+    BST(){
+        root = null;
     }
-    
-    private TNode insertRec(TNode node, int key){
-        if(node == null){
-            return new TNode(key);
+    tNode insertRec(tNode node, int key){
+        if(root == null){
+            root = new tNode (key);
+            return root;
         }
-        if (key<node.key){
-            node.left = insertRec(node.left, key);
-        }else if (key>node.key){
-            node.right  = insertRec(node.right, key);
-        }
-        return node;
+        if (key< root.key){
+            root.left = insertRec(root.left, key);
+            
+        else if (key > root.key)
+            root.right  = insertRec(root.right, key);
+        
+        return root;
     }
-    
-    public void insert(int key){
+    void insert(int key){
         root = insertRec(root, key);
-    }
-}                      //implemented BST core functionality
-public boolean isBST(TNode node, int min, int max){
-    if(node == null) return true;
-    if(node.key<min || node.key>max) return false;
-    return isBST(node.left, min, node.key - 1) &&  isBST(node.right, node.key + 1, max);
+    }          //implemented BST core functionality
+  boolean isBST(tNode node, int min, int max){
+    if(node == null)
+        return true;
+    if(node.key<min || node.key>max) 
+        return false;
+      
+    return isBST(node.left, min, node.key-1) &&  isBST(node.right, node.key+1, max);
 }                      //check BST property
-private int minValue(TNode node){
-    while(node.left!= null){
+int minValue(tNode node){
+    int min = node.key;
+    
+    while(node.left != null){
+        min = node.left.key;
         node = node.left;
     }
-    return node.key;
+    return min;
 }                     // find minimum value
 
-public TNode deleteRec(TNode node, int key){
-    if(node == null) return null;
-    
-    if(key<node.key){
-        node.left = deleteRec(node.left, key);
-    }else if(key>node.key){
-        node.right = deleteRec(node.right, key);
-    }else{
-        if(node.left == null) return node.right;
-        if(node.right == null) return node.left;
-        
-        node.key = minValue(node.right);
-        node.right = deleteRec(node.right, node.key);
-    }
-    return node;
-}                       //Added delete operation to BST
+tNode deleteRec(tNode node, int key){
+    if(root == null) 
+        return root;
+    if(key < root.key){
+        root.left = deleteRec(root.left, key);
+    else if(key > root.key)
+        root.right = deleteRec(root.right, key);
+    else{
+        if(root.left == null) 
+            return node.right;
+        else if(root.right == null) 
+            return root.left;
+        root.key = minValue(root.right);
+        root.right = deleteRec(root.right, root.key);
+     }
+     return root;
+  } 
+}                      //Added delete operation to BST
 
-Public class tryBST{
+ public class tryBST{
     static void buildBalanced(BST tree, int start, int end){
-        if(start>end) return;
-        
+        if(start > end) 
+            return;
         int mid = (start + end)/2;
         tree.insert(mid);
-        
         buildBalanced(tree, start, mid-1);
         buildBalanced(tree, mid+1, end);
     }                       // utility methods for building and modifying the tree
         
-    static void removeEvens(BST tree, int max){
-        for(int i = 2; i<=max; i+=2){
+  static void removeEvens(BST tree, int max){
+        for(int i=2; i<=max; i+=2){
             tree.root = tree.deleteRec(tree.root, i);
         }
-    }                       // remove even numbers
+  }                       // remove even numbers
      public static void main(String[] args){
-        
         int n = 18;
-        int max = (int) Math.pow(pow, n)-1;
-        
-        int repetition = 30;
-        
+        int max = (int) Math.pow(2, n)-1;
+        int repetitions = 30;
+
+         double [] populateTimes = new double[repetitions];
+         double [] deleteTimes = new double[repetitions];
+         
         long totalPopulate = 0;
         long totalDelete = 0;
         
-        for(int i =0; i<repetitions; i++){
-            
+        for(int i=0; i<repetitions; i++){
             BST tree = new BST();    //main method with benchmarking logic
+            long populateStart = System.currentTimeMillis();
+            buildBalanced(tree,1 ,max);
+            long populateEnd = System.currentTimeMillis();
+            
+            populateTotal += (populateEnd - populateStart);  //populate tree
+            
+            if(!tree.isBST(tree.root, Integer.MIN_VALUE, Integer.MAX_VALUE)){
+                System.out.println("Tree is not BST!");
+            }
+            
+            long deleteStart = System.currentTimeMillis();
+            removeEvenNumbers(tree, max);
+            long deleteEnd = System.currentTimeMillis();
+            deleteTotal += (deleteEnd - deleteStart);
+        }
+        double populateAvg = populateTotal/ (double) repetitions;
+        double deleteAvg = deleteTotal/(double) repetitions;
+        
+        System.out.println("\nMethod\t\t\tNumber of keys n\tAverage time(ms). \tStandard Deviation");
+        System.out.println("Populate tree\t\t" + max + "\t\t" + populateAvg + "\t\t" + stdPopulate);
+        System.out.println("Remove evens\t\t" + max + "\t\t" + deleteAvg + "\t\t\t" + stdDelete);
+    }
+}      
+    
+            
 
 
 
