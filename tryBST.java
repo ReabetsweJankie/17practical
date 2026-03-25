58f96e2a0d6570ad6f4acde4d59109408e323f39
ReabetsweJankie <jankiereabetswe@gmail.com>
2026-03-25 14:23:27 +0200

    Defined tree node structure

--- tryBST.java
+++ tryBST.java
@@ -1,1 +1,13 @@
 //created a java file
+import java.util.*;
+
+class TNode{
+    int key;
+    TNode left, right;
+    
+    public TNode(int key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
