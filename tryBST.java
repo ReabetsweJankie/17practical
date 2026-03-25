eb1bab52f6f9bc9edffbac595c5cfa740d2e388c
ReabetsweJankie <jankiereabetswe@gmail.com>
2026-03-25 14:33:50 +0200

    Implement BST core functionality

--- tryBST.java
+++ tryBST.java
@@ -10,4 +10,29 @@ class TNode{
         this.left = null;
         this.right = null;
     }
-}
+}                      // Defined tree node structure
+class BST{
+    
+    TNode root;
+    
+    public BST(){
+        this.root = null;
+    }
+    
+    private TNode insertRec(TNode node, int key){
+        if(node == null){
+            return new TNode(key);
+        }
+        if (key<node.key){
+            node.left = insertRec(node.left, key);
+        }else if (key>node.key){
+            node.right  = insertRec(node.right, key);
+        }
+        return node;
+    }
+    
+    public void insert(int key){
+        root = insertRec(root, key);
+    }
+}                
+    
